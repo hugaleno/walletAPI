362af6134264c1ea5ae5a7bf3a5ca2ea3a6901ff
Hugaleno Bezerra <hugaleno@gmail.com>
2020-12-05 23:01:21 -0300

    First commit

---
+++ src/main/java/br/com/hcb/wallet/application/WalletApplication.java
@@ -0,0 +1,11 @@
+package br.com.hcb.wallet.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WalletApplication.class);
+    }
+}
